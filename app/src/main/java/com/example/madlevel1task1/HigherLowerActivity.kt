6d7254e51dbc2cc410911f90f57a92ee41855621
Zakaria Chebti <zakaria@chipta.com>
2020-09-14 00:43:04 +0200

    Added listeners and there functions.

--- app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/com/example/madlevel1task1/HigherLowerActivity.kt
@@ -2,6 +2,7 @@ package com.example.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
@@ -12,7 +13,8 @@ class HigherLowerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initViews()
     }
 
@@ -20,6 +22,9 @@ class HigherLowerActivity : AppCompatActivity() {
      * Set the initial (UI) state of the game.
      */
     private fun initViews() {
+        binding.btnHigher.setOnClickListener { onHigherClick() }
+        binding.btnEqual.setOnClickListener { onEqualClick() }
+        binding.btnLower.setOnClickListener { onLowerClick() }
         updateUI()
     }
 
@@ -28,7 +33,66 @@ class HigherLowerActivity : AppCompatActivity() {
      */
     private fun updateUI() {
         binding.lastThrow.text = getString(R.string.last_throw, lastThrow)
+
+        when (currentThrow) {
+            1 -> binding.ImgDice.setImageResource(R.drawable.dice1)
+            2 -> binding.ImgDice.setImageResource(R.drawable.dice2)
+            3 -> binding.ImgDice.setImageResource(R.drawable.dice3)
+            4 -> binding.ImgDice.setImageResource(R.drawable.dice4)
+            5 -> binding.ImgDice.setImageResource(R.drawable.dice5)
+            6 -> binding.ImgDice.setImageResource(R.drawable.dice6)
+        }
     }
 
+    /**
+     * Replaces the previous dice value with the current one and replaces the current dice with a new dice
+     * with a random number between 1 and 6 (inclusive).
+     */
+    private fun rollDice() {
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+        updateUI()
+    }
+
+    /**
+     * Calls [rollDice] and checks if the answer is correct.
+     */
+    private fun onHigherClick() {
+        rollDice()
+        if (lastThrow < currentThrow) onAnswerCorrect()
+        else onAnswerIncorrect()
+    }
+
+    /**
+     * Calls [rollDice] and checks if the answer is correct.
+     */
+    private fun onLowerClick() {
+        rollDice()
+        if (lastThrow > currentThrow) onAnswerCorrect()
+        else onAnswerIncorrect()
+    }
+
+    /**
+     * Calls [rollDice] and checks if the answer is correct.
+     */
+    private fun onEqualClick() {
+        rollDice()
+        if (lastThrow == currentThrow) onAnswerCorrect()
+        else onAnswerIncorrect()
+    }
+
+    /**
+     * Displays a successful Toast message.
+     */
+    private fun onAnswerCorrect() {
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_SHORT).show()
+    }
+
+    /**
+     * Displays an incorrect Toast message.
+     */
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_SHORT).show()
+    }
 
 }
